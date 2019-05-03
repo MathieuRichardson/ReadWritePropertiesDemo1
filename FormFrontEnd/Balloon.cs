f0073a8a6b080073893e5c5cf0d41f370c1605ff
MathieuRichardson <MathieuRichardson1999@gmail.com>
2019-05-03 09:28:40 -0300

    progress

---
+++ FormFrontEnd/Balloon.cs
@@ -0,0 +1,96 @@
+﻿
+
+using System.Data;
+using System.Windows.Forms;
+
+namespace FormFrontEnd
+{
+    public class Balloon
+    {
+        private string colour;
+        //private decimal height;
+        private decimal diameter;
+
+       
+
+        public string Colour
+        {
+            get
+            {
+                return colour;
+            }
+            set
+            {
+                if(value.ToUpper()=="RED"|| value.ToUpper() == "BLUE")
+                {
+                    colour = value;
+                }
+                else
+                {
+                    throw new DataException("Color must be red or blue");
+                }
+                
+            }
+        }
+        public string BaloonName { get; set; }
+
+        public decimal Height
+        {
+            get;
+
+            set;
+           
+        }
+
+        public decimal Diamiter
+        {   
+            
+            get
+            {
+                return diameter;
+            }
+            set
+            {
+                //if(colour != string.Empty)
+                if(string.IsNullOrEmpty(colour))
+                {
+
+                    if (colour.ToUpper() == "BLUE")
+                    {
+                        
+                        if (diameter <= 5)
+                        {
+                            diameter = value;
+                        }
+                        else
+                        {
+                            throw new ConstraintException("blue baloon must be less than or equal to 5");
+                        }
+
+                    }
+                    else if ( colour.ToUpper() == "RED")
+                    {
+                        if(diameter >= 12)
+                        {
+                            diameter = value;
+                        }
+                        else
+                        {
+                            throw new ConstraintException("Red baloon must be more ten or equal to 12");
+                        }
+
+                    }
+
+ 
+                }
+                else
+                {
+                    throw new DataException("The color must be set first");
+                }
+                
+            }
+        }
+
+
+    }
+}
--- FormFrontEnd/Form1.cs
+++ FormFrontEnd/Form1.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FormFrontEnd
 {
     public partial class Form1 : Form
-    {     
+    {
+        Balloon myFirstBalloon;
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +14,35 @@ namespace FormFrontEnd
 
         private void btnSet_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                myFirstBalloon = new Balloon()
+                {
+                    Colour = txtColor.Text.Trim(),
+                    Diamiter = Convert.ToDecimal(txtDiameter.Text.Trim()),
+                    Height = Convert.ToDecimal(txtHeight.Text.Trim())
+                };
+                //myFirstBalloon.Colour = txtColor.Text.Trim();
+                //myFirstBalloon.Diamiter = Convert.ToDecimal(txtDiameter.Text.Trim());
+                //myFirstBalloon.Height = Convert.ToDecimal(txtHeight.Text.Trim());
+
+            }
+            catch(DataException ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString(),MessageBoxButtons.OK,MessageBoxIcon.Stop);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+
         }
 
         private void btnGet_Click(object sender, EventArgs e)
         {
-            
+            lblOutput.Text = $"color: {myFirstBalloon.Colour} " +
+                $"\nHeight: {myFirstBalloon.Height.ToString()}" +
+                $"\nDiameter: {myFirstBalloon.Diamiter.ToString()}";
         }
     }
 }
